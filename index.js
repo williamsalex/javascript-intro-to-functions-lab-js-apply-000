aef47f68b16489f787f1b8ce784ac32d7bff7505
Alexander Williams <williams_alex@protonmail.com>
2019-09-26 22:20:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ function sayHiToGrandma(string){
   } else if (string === "I love you, Grandma."){
     return "I love you, too."
   } else {
-    return "YES INDEED"
+    return "YES INDEED!"
   }
 }
